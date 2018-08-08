5d345ea5a4d815e58297f6d1a0f318a0f25666f9
mathiaso3110 <mathias@juloverby.dk>
2018-08-08 23:53:41 +0100

    support line working

--- supportResistanceFunction.py
+++ supportResistanceFunction.py
@@ -6,7 +6,10 @@ import matplotlib.pyplot as plt
 
 def getIndices(j):
 
-    return [i for i in range(len(j))]
+    return ([i for i in range(len(j))])
+
+def getPrices(j, prices):
+    return [prices[i] for i in j]
 
 def regressionAnalysis(x_values, y_values):
     tf.reset_default_graph()
@@ -43,23 +46,19 @@ def applyFunc(a, b, x):
     return a * x + b
 
 def splitList(indices, myList, a, b):
-    overPrices = []
-    overIndices = []
-    underPrices = []
-    underIndices = []
+    over = []
+    under = []
 
     for index in indices:
 
         price = myList[index]
 
         if price > applyFunc(a, b, index):
-            overPrices.append(price)
-            overIndices.append(index)
+            over.append(index)
         else:
-            underPrices.append(price)
-            underIndices.append(index)
+            under.append(index)
 
-    return overPrices, overIndices, underPrices, underIndices
+    return over, under
 
 def showPlot(indices, prices, *args):
     plt.plot(indices, prices)
@@ -75,36 +74,45 @@ prices = [1, 3, 2, 3, 4, 2, 5, 4, 3]
 
 # 1. regression analysis for all points
 indices = getIndices(prices)
-orgIndices = indices
 a1, b1 = regressionAnalysis(indices, prices)
 # showPlot(indices, prices, (a1, b1))
-overPrices, overIndices, underPrices, underIndices = splitList(indices, prices, a1, b1)
+over, under = splitList(indices, prices, a1, b1)
 
 # 2a. regression analysis for top points
 # Depending on the volatility of the stock adjust how many times you do regression analysis
 # Or change the b value to move the function down
 # Or move the function down no matter what to make sure also to get the points a bit under the function(change b)
-a2, b2 = regressionAnalysis(overIndices, overPrices)
-print(a2, b2)
-# showPlot(orgIndices, prices, (a1, b1), (a2, b2))
+overPrices = getPrices(over, prices)
+a2, b2 = regressionAnalysis(over, overPrices)
+
 
 # Gets the max top points for the second function
 
 topPoints = []
+tempPoints = []
+lastI = -1
 
-for index in overIndices:
-
-    price = prices[index]
-
-    tempPoints = []
-
-    if price > applyFunc(a2, b2, index):
-        tempPoints.append(index)
-    elif not len(tempPoints) == 0:
+for i in over:
+    if not len(tempPoints) == 0 and not lastI + 1 == i:
         topPoints.append(max(tempPoints))
         tempPoints = []
 
+    price = prices[i]
+
+    if price > applyFunc(a2, b2, i):
+        tempPoints.append((price, i))
+    elif not len(tempPoints) == 0:
+        topPoints.append(max(tempPoints))
+        tempPoints = []
+    
+    lastI = i
+
 print(topPoints)
+# showPlot(indices, prices, (a1, b1), (a2, b2))
 
+spikesPrices = [i[0] for i in topPoints]
+spikesIndices = [i[1] for i in topPoints]
+print(spikesIndices)
 
-
+a3, b3 = regressionAnalysis(spikesIndices, spikesPrices)
+showPlot(indices, prices, (a3, b3))
